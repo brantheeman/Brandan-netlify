7148f16e38554d14247479fa22f4fdc3e907b5be
Brandan Pratt <brandan.pratt1@gmail.com>
2022-04-16 23:58:17 -0400

    No changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,10 @@
 import React, {useState} from "react";
-import { transition } from "@headlessui/react";
-import Link from "react-scroll/modules/components/Link";
+import { Transition } from "@headlessui/react";
+import { Link } from "react-scroll";
+import Image from "next/image";
 
 function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setisOpen] = useState(false);
     return (
         <div>
             <nav className='fixed z-20 bg-white w-full'>
@@ -33,37 +34,50 @@ function Navbar() {
                             </div>
                         </div>
                         <div className="mr-14 flex md:hidden">
-                            <button onClick={() => setIsOpen(!isOpen)} type="button" className="bg-blue-600 inline-flex items-centerjustify-center p-2 rounded-md text-white hover:bg-black focus:outline-none focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                            <button onClick={() => setisOpen(!isOpen)} type="button" className="bg-blue-600 inline-flex items-center justify-center p-2 rounded-md text-white hover:bg-black focus:outline-none focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                                 <span className="sr-only">Open main menu</span>
-                                {!isOpen ? (<svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="no" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                {!isOpen ? (
+                                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="no" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6H16M4 12h16M4 18h16"/>
                                 </svg>
-                                ) : (
-                                    <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="no" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6112 12"/>
-                                    </svg>
+                                ) 
+                                : 
+                                (
+                                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="no" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"/>
+                                </svg>
                                 )}
                             </button>
                         </div>
                     </div>
                 </div>
-                <transition show={isOpen} enter="transistion ease-out duration-100 transform" enterFrom="opacity-0 scale-95" enterTo="opacity-100 scale-100" leave="transistion ease-in duration-75 transform" leaveFrom="opacity-100 scale-100" leaveto="opacity-0 scale-95">
+                {/*small size: mobile div start */}
+                {/*Transistion properties for smooth transistion */}
+                <Transition 
+                show={isOpen} 
+                enter="transistion ease-out duration-100 transform" 
+                enterFrom="opacity-0 scale-95" 
+                enterTo="opacity-100 scale-100" 
+                leave="transistion ease-in duration-75 transform" 
+                leaveFrom="opacity-100 scale-100" 
+                leaveTo="opacity-0 scale-95"
+                >
                 {(ref) => (
-                    <div className="md:hidden" id="mobile-menu">
-                        <div ref={ref} className="bg-white mx-4 mr-20 pt-4 pb-4 space-y-1">
-                            <Link href="/home" activeClass="home" smooth={true} offset={50} duration={500} className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</Link>
-                            <Link href="/services" activeClass="services" smooth={true} offset={50} duration={500} className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">Services</Link>
-                            <Link href="/work" activeClass="work" smooth={true} offset={50} duration={500} className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">Work</Link>
-                            <Link href="/Clients" activeClass="Clients" smooth={true} offset={50} duration={500} className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">Clients</Link>
-                            <Link href="/contacts" activeClass="contacts" smooth={true} offset={50} duration={500} className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contacts</Link>
-                            <Link href="/Me" activeClass="Me" to="Me" smooth={true} offset={50} duration={500} className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact<span className="text-black">Me</span></Link>
-                        </div>
-                    </div>
-                )}
-                </transition>
+                            <div className="md:hidden" id="mobile-menu">
+                                <div ref={ref} className="bg-white mx-4 mr-20 pt-4 pb-4 space-y-1">
+                                    <Link href="/home" activeClass="home" smooth={true} offset={50} duration={500} className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</Link>
+                                    <Link href="/services" activeClass="services" smooth={true} offset={50} duration={500} className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">Services</Link>
+                                    <Link href="/work" activeClass="work" smooth={true} offset={50} duration={500} className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">Work</Link>
+                                    <Link href="/Clients" activeClass="Clients" smooth={true} offset={50} duration={500} className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">Clients</Link>
+                                    <Link href="/contacts" activeClass="contacts" smooth={true} offset={50} duration={500} className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contacts</Link>
+                                    <Link href="/Me" activeClass="Me" to="Me" smooth={true} offset={50} duration={500} className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact<span className="text-black">Me</span></Link>
+                                </div>
+                            </div>
+                        )}
+                </Transition>
             </nav>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
