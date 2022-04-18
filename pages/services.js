e9e2b1dd333021c9a22bee028e3ecb5a92cd4a6a
Brandan Pratt <brandan.pratt1@gmail.com>
2022-04-17 20:34:28 -0400

    Swapped out services for programming languages

--- pages/services.js
+++ pages/services.js
@@ -1,56 +1,52 @@
 import React from "react";
 import {
-  MdWeb,
-  MdOutlineDesignServices,
-  MdChatBubbleOutline,
-} from "react-icons/md";
-import { AiOutlineAppstoreAdd } from "react-icons/ai";
-import { GiGrowth } from "react-icons/gi";
-import { SiAzuredevops } from "react-icons/si";
+    AiOutlineConsoleSql,
+} from "react-icons/ai";
+import { SiHtml5, SiCss3, SiJavascript, SiReact, SiGit } from "react-icons/si";
 
 function services() {
   return (
     <div className="flex-col justify-center items-center my-20">
-      <p className="text-violet-500 uppercase text-center">Services</p>
+      <p className="text-violet-500 uppercase text-center">Languages</p>
       <h2 className="text-5xl  text-center">
         My Programming<span className="font-bold">Languages</span>
       </h2>
       <div className="flex justify-center items-center">
         <div className="grid grid-cols-2 md:mx-0 mx-10 md:grid-cols-3 gap-5 my-20">
           <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-violet-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
-            <MdWeb className="text-4xl text-gray-600 group-hover:text-white" />
+            <SiHtml5 className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
-              Web Development
+              HTML5
             </h1>
           </div>
           <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-violet-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
-            <AiOutlineAppstoreAdd className="text-4xl text-gray-600 group-hover:text-white" />
+            <SiCss3 className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
-              App Development
+              CSS3
             </h1>
           </div>
           <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-violet-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
-            <MdOutlineDesignServices className="text-4xl text-gray-600 group-hover:text-white" />
+            <SiJavascript className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
-              UI/UX Design
+              JavaScript
             </h1>
           </div>
           <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-violet-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
-            <MdChatBubbleOutline className="text-4xl text-gray-600 group-hover:text-white" />
+            <AiOutlineConsoleSql className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
-              AI Chatbot
+              SQL
             </h1>
           </div>
           <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-violet-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
-            <GiGrowth className="text-4xl text-gray-600 group-hover:text-white" />
+            <SiReact className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
-              Growth Hacking
+              React
             </h1>
           </div>
           <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-violet-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
-            <SiAzuredevops className="text-4xl text-gray-600 group-hover:text-white" />
+            <SiGit className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
-              Devops
+              Git
             </h1>
           </div>
         </div>
