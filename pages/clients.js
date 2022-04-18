32cb8fef05a34b4fc2a1965efd260269a7099784
Brandan Pratt <brandan.pratt1@gmail.com>
2022-04-17 20:33:12 -0400

    Turned this page into an about section

--- pages/clients.js
+++ pages/clients.js
@@ -10,17 +10,16 @@ import {
 function clients() {
   return (
     <div className="flex-col justify-center items-center my-40">
-      <p className="text-violet-500 uppercase text-center">Clients</p>
+      <p className="text-violet-500 uppercase text-center">About</p>
       <h2 className="text-5xl   text-center">
-        Awesome <span className="font-bold"> Clients</span>
+        Who is<span className="font-bold"> Brandan?</span>
       </h2>
       <div className="flex justify-center items-center">
-        <div className="grid md:grid-cols-5 grid-cols-3 mt-10 gap-8">
-          <FaMicrosoft className="text-6xl text-gray-300 cursor-pointer hover:text-black" />
-          <FaDiscord className="text-6xl text-gray-300 cursor-pointer hover:text-black" />
-          <FaApple className="text-6xl text-gray-300 cursor-pointer hover:text-black" />
-          <FaAmazon className="text-6xl text-gray-300 cursor-pointer hover:text-black" />
-          <FaUber className="text-6xl text-gray-300 cursor-pointer hover:text-black" />
+        <div className="justify-self-auto ">
+          <p className="text-4xl text-center w-auto h-auto">
+              I am a problem solver, a builder, I am a Software Engineer.{'\n'}
+              I chose Programming because I had a love for videogames.
+          </p>
         </div>
       </div>
     </div>
