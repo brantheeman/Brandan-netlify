08ae506132adc9e33a46dc97fbd0a0cf064383e3
Brandan Pratt <brandan.pratt1@gmail.com>
2022-04-18 20:56:22 -0400

    Gave the Languages unique colors

--- pages/services.js
+++ pages/services.js
@@ -13,37 +13,37 @@ function services() {
       </h2>
       <div className="flex justify-center items-center">
         <div className="grid grid-cols-2 md:mx-0 mx-10 md:grid-cols-3 gap-5 my-20">
-          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-violet-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
+          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-orange-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
             <SiHtml5 className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
               HTML5
             </h1>
           </div>
-          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-violet-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
+          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-blue-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
             <SiCss3 className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
               CSS3
             </h1>
           </div>
-          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-violet-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
+          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-yellow-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
             <SiJavascript className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
               JavaScript
             </h1>
           </div>
-          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-violet-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
+          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-green-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
             <AiOutlineConsoleSql className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
               SQL
             </h1>
           </div>
-          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-violet-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
+          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-blue-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
             <SiReact className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
               React
             </h1>
           </div>
-          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-violet-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
+          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-orange-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
             <SiGit className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
               Git
