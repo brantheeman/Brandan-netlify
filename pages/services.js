ce6bab0dd864ea17308a54936aca69b36a7e4fa9
Brandan Pratt <brandan.pratt1@gmail.com>
2022-04-21 20:47:23 -0400

    Added Flutter, GraphQL, and Vue

--- pages/services.js
+++ pages/services.js
@@ -2,14 +2,14 @@ import React from "react";
 import {
     AiOutlineConsoleSql,
 } from "react-icons/ai";
-import { SiHtml5, SiCss3, SiJavascript, SiReact, SiGit } from "react-icons/si";
+import { SiHtml5, SiCss3, SiJavascript, SiReact, SiGit, SiGraphql, SiFlutter, SiVuedotjs } from "react-icons/si";
 
 function services() {
   return (
     <div className="flex-col justify-center items-center my-20">
-      <p className="text-violet-500 uppercase text-center">Languages</p>
+      <p className="text-violet-500 uppercase text-center">Languages & Frameworks</p>
       <h2 className="text-5xl  text-center">
-        My Programming<span className="font-bold">Languages</span>
+        My <span className="font-bold">Languages</span>
       </h2>
       <div className="flex justify-center items-center">
         <div className="grid grid-cols-2 md:mx-0 mx-10 md:grid-cols-3 gap-5 my-20">
@@ -47,6 +47,24 @@ function services() {
             <SiGit className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
               Git
+            </h1>
+          </div>
+          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-pink-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
+            <SiGraphql className="text-4xl text-gray-600 group-hover:text-white" />
+            <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
+              GraphQL
+            </h1>
+          </div>
+          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-blue-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
+            <SiFlutter className="text-4xl text-gray-600 group-hover:text-white" />
+            <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
+              Flutter
+            </h1>
+          </div>
+          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-green-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
+            <SiVuedotjs className="text-4xl text-gray-600 group-hover:text-white" />
+            <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
+              Vue
             </h1>
           </div>
         </div>
