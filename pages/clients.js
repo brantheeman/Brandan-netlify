8c602567a7fc6a05a9ae01196ed12d4457a98eb3
Brandan Pratt <brandan.pratt1@gmail.com>
2022-05-01 12:47:21 -0400

    Punctuation error

--- pages/clients.js
+++ pages/clients.js
@@ -13,7 +13,7 @@ function clients() {
               I am an experienced Software Engineer located in Nassau, Bahamas. I am pursuing my Associates Degree in Software Engineering.
               I have been a Software Developer for one year now, and I am open to working on projects of all sorts, mainly in Web Development.
               I am proficient with HTML5, CSS3, and JavaScript, and I am currently developing my skills with React and Vue. I am knowlegeable about database management, python, and java, but I mainly focus on web technologies.
-              If you're interested in working with me, feel free to contact me to get in touch, I look forward to helping your project come to life.
+              If you're interested in working with me, feel free to contact me to get in touch. I look forward to helping your project come to life.
           </p>
         </div>
       </div>
