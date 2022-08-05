57afc461c32e5c035bd32f1c4c355ae3ec2a7f96
Brandan <brandan.pratt1@gmail.com>
2022-08-05 10:28:21 -0400

    Added Laravel to programming language

--- pages/services.js
+++ pages/services.js
@@ -2,7 +2,7 @@ import React from "react";
 import {
     AiOutlineConsoleSql,
 } from "react-icons/ai";
-import { SiHtml5, SiCss3, SiJavascript, SiReact, SiGit} from "react-icons/si";
+import { SiHtml5, SiCss3, SiJavascript, SiLaravel, SiGit} from "react-icons/si";
 
 function services() {
   return (
@@ -37,10 +37,10 @@ function services() {
               SQL
             </h1>
           </div>
-          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-blue-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
-            <SiReact className="text-4xl text-gray-600 group-hover:text-white" />
+          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-red-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
+            <SiLaravel className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
-              React
+              Laravel
             </h1>
           </div>
           <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-orange-500 hover:shadow-lg  border-violet-100 rounded-md p-8 hover:border-violet-500">
